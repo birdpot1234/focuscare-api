cf0f7e98eb857384e0aa217ee7a3afc9236ab23d
zinadev <zinadeveloper@gmail.com>
2019-05-15 15:17:55 +0700

    battery

--- src/monitor/routes.js
+++ src/monitor/routes.js
@@ -15,4 +15,12 @@ router.post("/save_screen",
         res.status(200).json({ success: true })
     })
 
+router.post('/battery',
+    validate_token(),
+    (req, res) => {
+        console.log(req.body)
+        res.status(200).json({ success: true });
+    }
+)
+
 module.exports = router;
